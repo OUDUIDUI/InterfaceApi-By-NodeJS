c15ebce31dafef54e63145919498d964938443b2
oushihao <oushihao97@163.com>
2020-08-20 18:03:03 +0800

    基础配置

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db.js');
+
+// 引入路由文件
+const product = require('./routes/product.js');
 
 dotenv.config({
     path:"./config/config.env"
 })
 
+// 链接数据库
+connectDB();
+
 const app = express();
+
+// 创建中间件
+app.use(morgan("dev"));
+
+app.get("/",(req,res)=>{
+    res.status(200).json({success:true,msg:"HelloWorld"});
+})
+
+// 挂载路由节点
+app.use("/api/v1/product",product);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta.bold)
 );
+
+process.on("unhandledRejection",(err,promise) => {
+    console.log(`Error:${err.message}`.red.bold);
+    // 关闭服务器 & 退出进程
+    server.close(()=>{
+        process.exit(1);
+    });
+})
